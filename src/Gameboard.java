bc11a9515ec2ec6ef1cc5dc8e933c9aa7f5705e7
Sami Kotkavuori <sami.kotkavuori@cs.tamk.fi>
2016-11-24 16:07:24 +0200

    Gameboard: add accessors for attributes

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -1,4 +1,24 @@
 public class Gameboard {
     private int width;
     private int height;
+
+    public void setWidth(int w){
+        if(w > 0 && w <= 80){
+            width = w;
+        }
+    }
+    
+    public int getWidth(){
+        return width;
+    }
+
+    public void setHeight(int h){
+        if(h > 0 && h <= 80){
+            height = h;
+        }
+    }
+
+    public int getHeight(){
+        return height;
+    }
 }
