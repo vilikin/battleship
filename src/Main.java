49c20aa5ba8890809580629adc4e4f8c9e9b7718
Erika Sankari <8eikochan@gmail.com>
2016-11-24 12:10:08 +0200

    Initialize Main class.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * TODO Short Description
+ * <p>
+ * TODO description and @since
+ *
+ * @author Erika Sankari
+ * @version 2016.1124
+ * @since 1.7
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
