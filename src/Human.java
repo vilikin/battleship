fba6679e79879d151116f4a84b0b6b6be1072a3c
Sankari <8eikochan@gmail.com>
2016-11-24 22:38:40 +0200

    Human: Initialize Human class.

---
+++ src/Human.java
@@ -0,0 +1,14 @@
+/**
+ * TODO Short Description
+ * <p>
+ * TODO description and @since
+ *
+ * @author Erika Sankari
+ * @version 2016.1124
+ * @since 1.7
+ */
+public class Human {
+    public Human() {
+
+    }
+}
