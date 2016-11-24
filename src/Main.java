aa2837a98b59d81cb06798e9f037da864a3ac4e7
Erika Sankari <8eikochan@gmail.com>
2016-11-24 12:15:01 +0200

    Main.main(): Add javadoc.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@
  * @since 1.7
  */
 public class Main {
+    /**
+     * Acts as a starting point of the program.
+     *
+     * @param args not used
+     */
     public static void main(String[] args) {
 
     }
