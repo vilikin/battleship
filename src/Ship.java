38e0961985109861bc797e01cd4c575b243412e0
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2016-11-24 15:24:02 +0200

    Ship: Add field size.

--- src/Ship.java
+++ src/Ship.java
@@ -6,4 +6,14 @@
  * @since 1.7
  */
 public class Ship {
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
