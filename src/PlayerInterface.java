571c5879b5849d6d7c80328a20172e25e198d956
Akash Singh <akash.singh@cs.tamk.fi>
2016-11-24 21:49:22 +0200

    PlayerInterface: Add interface javadoc.

--- src/PlayerInterface.java
+++ src/PlayerInterface.java
@@ -1,5 +1,10 @@
-interface PlayerInterface {
-
-
+/**
+ * PlayerInterface includes simple methods for player and computer.
+ *
+ * @author Akash Singh
+ * @version 2016.1124
+ * @since 1.7
+ */ 
+public interface PlayerInterface {
 
 }
