91ecc1aeb6db07fa1ec91abbc37433eae910a3c0
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2016-11-26 21:27:03 +0200

    Modify Computer-object to implement PlayerInterface.

--- src/Computer.java
+++ src/Computer.java
@@ -2,10 +2,10 @@
  * Computer player for the game.
  *
  * @author Valtteri Poutanen valtteri.poutanen@hotmail.com
- * @version 2016.1124
+ * @version 2016.1126
  * @since 1.7
  **/
-public class Computer {
+public class Computer implements PlayerInterface {
 
     /**
      * Default constructor for Computer.
