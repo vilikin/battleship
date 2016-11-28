8cabb4e3416719cee35f6c7f16323d24e8155b62
Vili Kinnunen <vili.kinnunen1@gmail.com>
2016-11-28 18:34:08 +0200

    Highscore: Add class javadoc.

--- src/Highscore.java
+++ src/Highscore.java
@@ -1,3 +1,10 @@
+/**
+ * Implements one highscore instance with name and score.
+ *
+ * @author Vili Kinnunen
+ * @version 2016.1128
+ * @since 1.8
+ */
 public class Highscore {
     private String name;
     private int score;
