aaa8278c382c2d449ac7b46a1a1133c7bc33b308
Sami Kotkavuori <sami.kotkavuori@cs.tamk.fi>
2016-11-29 20:36:04 +0200

    Gameboard: Modify class to use GameboardField.

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -1,56 +1,14 @@
 public class Gameboard {
-    private int width;
-    private int height;
-    private char[][] board;
+    private GameboardField field1;
+    private GameboardField field2;
 
-    public void setWidth(int w){
-        if(w > 0 && w <= 80){
-            width = w;
-        }
-    }
-    
-    public int getWidth(){
-        return width;
-    }
-
-    public void setHeight(int h){
-        if(h > 0 && h <= 80){
-            height = h;
-        }
-    }
-
-    public int getHeight(){
-        return height;
-    }
-
-    public Gameboard(int width, int height){
-        setWidth(width);
-        setHeight(height);
-        createNewEmptyBoard();
-    }
-
-    public void createNewEmptyBoard() {
-        board = new char[height][width];
-        for (int i= 0; i < height; i++) {
-            for (int j = 0; j < width ; j++) {
-                board[i][j] = ' ';
-            }
-        }
-    }
-
-    public String drawBoard(){
-        String result = "";
-        for( int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++) {
-                result += board[i][j];
-            }
-            result += "\n";
-        }
-        return result;
+    public Gameboard(){
+        field1 = new GameboardField(10,10);
+        field2 = new GameboardField(10,10);
     }
 
     @Override
     public String toString(){
-        return drawBoard();
+        return field1 + "\n" + field2;
     }
 }
