da2f8570f735ec3f50367ebcb0647b01ac075801
Sami Kotkavuori <sami.kotkavuori@cs.tamk.fi>
2016-11-29 20:03:40 +0200

    GameboardField: Add field size attributes. GameboardField: Add table to store data on field. GameboardField: Add constructor.

--- src/GameboardField.java
+++ src/GameboardField.java
@@ -2,5 +2,13 @@
  * Created by c5skotka on 29.11.2016.
  */
 public class GameboardField {
-    
+    private int width;
+    private int height;
+    private char[][] field;
+
+    public GameboardField(int width, int height) {
+        this.width = width;
+        this.height = height;
+        field = new char[width][height];
+    }
 }
