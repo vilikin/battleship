22f6316e5cbd894e5f30afa978a02469985524ed
Sami Kotkavuori <sami.kotkavuori@cs.tamk.fi>
2016-11-29 20:06:10 +0200

    GameboardField: Add accessors for field size attributes.

--- src/GameboardField.java
+++ src/GameboardField.java
@@ -6,6 +6,26 @@ public class GameboardField {
     private int height;
     private char[][] field;
 
+    public void setWidth(int w){
+        if(w > 0 && w <= 80){
+            width = w;
+        }
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public void setHeight(int h){
+        if(h > 0 && h <= 80){
+            height = h;
+        }
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
     public GameboardField(int width, int height) {
         this.width = width;
         this.height = height;
