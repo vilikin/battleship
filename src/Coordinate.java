ffba69e6179033f92210ae9512f344b7f77cfe5b
Sankari <8eikochan@gmail.com>
2016-11-30 19:37:35 +0200

    Coordinate: Create constructor Coordinate(int x, int y).

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -19,6 +19,11 @@ public class Coordinate {
      */
     private int y;
 
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     /**
      * Returns coordinate x.
      *
