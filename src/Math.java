219ebe51fec4010e4f29b32f9f4f38e770e29d67
Akash Singh <akash.singh@cs.tamk.fi>
2016-12-08 14:08:00 +0200

    Math.plus(int x, int y): Add method logic.

--- src/Math.java
+++ src/Math.java
@@ -5,7 +5,15 @@
  */
 public class Math {
 
+    /**
+     * Add two integer values together and prints result.
+     *
+     * @param number1 integer value
+     * @param number2 integer value
+     */
     public void plus(number1, number2) {
-
+        int result = number1 + number2;
+        System.out.println(result);
     }
+
 }
