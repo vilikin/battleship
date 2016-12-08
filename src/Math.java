33e75a3883f96116b412a35eeae64af858bfa145
Akash Singh <akash.singh@cs.tamk.fi>
2016-12-08 14:19:43 +0200

    Math.minus(int x, int y): Add method logic.

--- src/Math.java
+++ src/Math.java
@@ -23,6 +23,7 @@ public class Math {
      * @param number2 subtraction number
      */
     public void minus(int number1, int number2) {
-        
+        int result = number1 - number2;
+        System.out.println(result);
     }
 }
