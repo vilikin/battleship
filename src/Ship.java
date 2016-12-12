f0183ceb8b213504c0741b98c55ce5a5a9f1d4af
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2016-12-12 13:18:33 +0200

    Ship: Fix faulty class javadoc.
    Removed '<' and '>' characters from class javadoc email, because
    javadoc does not allow these characters.

--- src/Ship.java
+++ src/Ship.java
@@ -6,8 +6,8 @@ import java.util.Map;
 /**
  * Represents a battleship on the game board.
  *
- * @author Toni Seppalainen <toni.seppalainen@cs.tamk.fi>
- * @version 2016.1124
+ * @author Toni Seppalainen toni.seppalainen@cs.tamk.fi
+ * @version 2016.1212
  * @since 1.7
  */
 public class Ship {
